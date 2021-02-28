2bbbeedc62abebb786828021668d9ed3f75c7eb1
KACPER CIERPIAK <KACPER_CIERPIAK@jabil.com>
2021-02-28 09:37:41 +0100

    Init unit tests

---
+++ ConfigService/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConfigService
+{
+    public class DbCredentials
+    {
+        public string dbHost { get; set; } = "";
+        public string dbPort { get; set; } = "";
+        public string dbName { get; set; } = "";
+        public string dbUsername { get; set; } = "";
+        public string dbPassword { get; set; } = "";
+
+        public DbCredentials(string uri)
+        {
+            var parsVal = new Uri(uri);
+            var userinfo = parsVal.UserInfo.Split(':');
+            dbHost = parsVal.Host;
+            dbPort = parsVal.Port.ToString();
+            dbName = parsVal.Segments[1];           
+            dbUsername = userinfo[0];
+            dbPassword = userinfo[1];
+        }
+
+    }
+    public class Settings
+    {
+        public string GetDBConnectionString()
+        {
+            var dbHost = Environment.GetEnvironmentVariable("PORT");
+            var dbPort = "5433";
+            var dbName = "";
+            var dbUsername = "";
+            var dbPassword = "";
+            return $"Host = {dbHost}; Port = {dbPort}; Database = {dbName}; Username = {dbUsername}; Password = {dbPassword}";
+
+        }
+    }
+}
---
+++ WebAPI.Test/ServicesTests/ConfigServiceTest.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+using ConfigService;
+
+namespace WebAPI.Test
+{
+    public class ConfigServiceTest
+    {
+     
+        [Fact]
+        public void CredentialMappingTest()
+        {
+            
+            var dbHost = "Host";
+            var dbPort = "5433";
+            var dbName = "testDb!1";
+            var dbUsername = "username1!A";
+            var dbPassword = "password1!A";
+            var URI = $"postgres://{dbUsername}:{dbPassword}@{dbHost}:{dbPort}/{dbName}";
+
+            var dbCredential = new DbCredentials(URI);
+
+            Assert.Equal(dbHost.ToLower(), dbCredential.dbHost);
+            Assert.Equal(dbPort, dbCredential.dbPort);
+            Assert.Equal(dbName, dbCredential.dbName);
+            Assert.Equal(dbUsername, dbCredential.dbUsername);
+            Assert.Equal(dbPassword, dbCredential.dbPassword);
+
+        }
+    }
+}
