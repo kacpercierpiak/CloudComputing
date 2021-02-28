05d8e2717aafd5bed239e84b0c3a2b7b7471660a
Arkadiusz Biesiada <arkbie002@o365.student.utp.edu.pl>
2021-02-28 18:57:47 +0100

    Add Users Models

---
+++ SPA/Services/UsersService.cs
@@ -0,0 +1,19 @@
+﻿using ConfigService.Models.DTO;
+using SPA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPA.Services
+{
+    public class UsersService
+    {
+        private UsersRepository _usersRepository { get; set; }
+        public UsersService()
+        {
+            _usersRepository = new UsersRepository();
+        }
+        public List<User> GetUsers() { return _usersRepository.GetUsers(); }
+    }
+}
